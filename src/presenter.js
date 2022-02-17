61fd3be5883749eff0e6d59e2e9ad96ebcd094d3
sebastian <sabestech@gmail.com>
2022-02-16 22:55:56 -0400

    hola implementado

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import sumar from "./sumador";
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const form_s = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
